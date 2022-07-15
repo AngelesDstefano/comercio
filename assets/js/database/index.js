561b304e21c1303e9dfff840f6ac94ba9464609d
AngelesDstefano <angelesdstefano@gmail.com>
2022-07-14 22:51:39 -0300

    versiondefi

---
+++ assets/js/database/index.js
@@ -0,0 +1,52 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Trimove Worm ",
+    precio: 120,
+    imagen: "./assets/images/modelo1.webp",
+    categoria: "bici",
+    cantidad: 15,
+  },
+  {
+    id: 2,
+    nombre: "FAT E-BOY X8",
+    precio: 140,
+    imagen: "./assets/images/modelo2.jpg",
+    categoria: "bici",
+    cantidad: 10,
+  },
+  {
+    id: 3,
+    nombre: " 20 Gtc 20 11",
+    precio: 110,
+    imagen: "./assets/images/modelo3.webp",
+    categoria: "bici",
+    cantidad: 10,
+  },
+  {
+    id: 4,
+    nombre: "Urban S6 Rembrandt 6v",
+    precio: 90,
+    imagen: "./assets/images/modelo4.webp",
+    categoria: "bici",
+    cantidad: 10,
+  },
+  {
+    id: 5,
+    nombre: "RODADO 20",
+    precio: 150,
+    imagen: "./assets/images/modelo5.jpg",
+    categoria: "bici",
+    cantidad: 10,
+  },
+  {
+    id: 6,
+    nombre: " R24 Fat Max-you",
+    precio: 70,
+    imagen: "./assets/images/modelo6.webp",
+    categoria: "bici",
+    cantidad: 10,
+  },
+]
+
+export default productos
